87dd38f072c889cc322a34353fccc44f2ffd5eac
SherryNinja10 <aarshdeepsinghvandal@gmail.com>
2025-05-13 17:03:41 -0700

    added the functinality to login. Log out still needs to be done through deleting the cookie in the application tab of dev tools.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 
-export default function LoginRolePage() {
+export default function LoginPage() {
   const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <main className="min-h-screen bg-white text-black font-sans flex flex-col">
@@ -13,22 +16,35 @@ export default function LoginRolePage() {
       </div>
 
       {/* Content */}
-      <div className="flex flex-col items-center justify-center flex-grow space-y-6 mt-10 text-center">
-        <h2 className="text-xl font-semibold">Log In as:</h2>
+      <div className="flex flex-col items-center justify-center flex-grow space-y-4 mt-10 text-center">
+        <h2 className="text-green-500 text-lg font-semibold"><br />Log In</h2>
+
+        <form action='/api/login' method='POST' className="flex flex-col gap-4">
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          className="bg-gray-200 text-center py-2 px-4 w-64 rounded"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          className="bg-gray-200 text-center py-2 px-4 w-64 rounded"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
         <button
-          className="bg-blue-500 text-white font-bold py-2 px-8 rounded"
-          onClick={() => router.push("/login/organization")}
+          type="submit"
+          className="bg-gray-200 py-2 px-8 rounded"
         >
-          Organization
+          Log in
         </button>
-
-        <button
-          className="bg-yellow-400 text-black font-bold py-2 px-8 rounded"
-          onClick={() => router.push("/login/member")}
-        >
-          Member
-        </button>
+        </form>
       </div>
 
       {/* Bottom Nav */}
