37ead1975412b089d9c9551a37b59aa1dd0a284c
jackyfhuang <jackyfhuang@gmail.com>
2025-05-16 11:36:15 -0700

    Reorganized routes for /Organization

--- app/components/navbar/organization/footer.tsx
+++ app/components/navbar/organization/footer.tsx
@@ -6,19 +6,19 @@ import { Home, MapPin, PlusCircle } from "lucide-react";
 export default function Footer() {
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-blue-500 py-3 px-6 flex justify-around items-center border-t border-gray-400 z-50">
-      <Link href="/organization/map">
+      <Link href="/Organization/map">
         <div className="flex flex-col items-center">
           <MapPin size={30} />
         </div>
       </Link>
 
-      <Link href="/organization">
+      <Link href="/Organization">
         <div className="flex flex-col items-center">
           <Home size={30} />
         </div>
       </Link>
 
-      <Link href="/organization/resources">
+      <Link href="/Organization/resources">
         <div className="flex flex-col items-center">
           <PlusCircle size={30} />
         </div>
