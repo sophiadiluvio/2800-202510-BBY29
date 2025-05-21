c3c121f547eb8bee8a09d29e676906a14042a5b0
RussellMelchiorre <rmelchiorre@my.bcit.ca>
2025-05-21 15:43:40 -0700

    changed and added comments to inventory bar and inventory grid to remove the background overflow when item types are not multiples of 4, Updated searchNav to have a womens shelter icon and rescources near you page.

--- app/components/searchNav.tsx
+++ app/components/searchNav.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRouter, usePathname } from 'next/navigation';
-import { FaHeart, FaTshirt, FaHome, FaUtensils } from 'react-icons/fa';
+import { FaHeart, FaTshirt, FaHome, FaFemale } from 'react-icons/fa';
 import { GiCannedFish } from "react-icons/gi";
 type Props = {
   userLocation: { lat: number; lng: number } | null;
@@ -41,16 +41,13 @@ export default function SearchNav({ userLocation }: Props) {
     router.push(cleanUrl);
   }
   
-  
-  
-  
 
   return (
     <div className="w-full bg-white px-4 py-2 flex justify-around items-center shadow-md border-t border-gray-200">
 
-      <button onClick={() => handleClick('favorites')} className="flex flex-col items-center text-xs text-gray-800">
-        <FaHeart className="text-2xl mb-1 text-red-500" />
-        <span>Favorites</span>
+      <button onClick={() => handleClick('women')} className="flex flex-col items-center text-xs text-gray-800">
+        <FaFemale className="text-2xl mb-1 text-red-500" />
+        <span>Womens Shelter</span>
       </button>
 
       <button onClick={() => handleClick('food')} className="flex flex-col items-center text-xs text-gray-800">
