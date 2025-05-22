4c6b70022330cea013d46cc5cb4897a5e9b28b90
Jack Huang <jackyfhuang@gmail.com>
2025-05-22 01:40:25 -0700

    Changed autorefresh to only refresh upon update, also added input validation to always be non negative.

--- app/components/itemTextBox.tsx
+++ app/components/itemTextBox.tsx
@@ -9,10 +9,16 @@ interface ItemTextBoxProps {
 }
 
 export default function ItemTextBox({ itemKey, initialValue, onUpdate }: ItemTextBoxProps) {
-  const [stockInput, setStockInput] = useState(initialValue);
+  const [stockInput, setStockInput] = useState(initialValue.toString());
 
   const handleUpdate = async () => {
-    await updateInvItem(itemKey, stockInput);
+    const parsed = parseInt(stockInput, 10)
+    if (isNaN(parsed)|| parsed < 0) {
+      alert('Please enter a valid number.');
+      return;
+    }
+    
+    await updateInvItem(itemKey, parsed);
     onUpdate();
   };
 
@@ -23,7 +29,7 @@ export default function ItemTextBox({ itemKey, initialValue, onUpdate }: ItemTex
         min='0'
         className='w-20 px-2 py-1 border rounded'
         value={stockInput}
-        onChange={(e) => setStockInput(parseInt(e.target.value, 10))}
+        onChange={(e) => setStockInput(e.target.value)}
       />
       <button className='bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600' onClick={handleUpdate}>
         Set
