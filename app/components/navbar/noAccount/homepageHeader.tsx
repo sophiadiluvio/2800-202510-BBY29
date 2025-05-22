996c71b4254a5f3cc577f26558feb17a693e5536
Jack Huang <jackyfhuang@gmail.com>
2025-05-22 13:34:55 -0700

    changed logo, question mark prompt for more info

--- app/components/navbar/noAccount/homepageHeader.tsx
+++ app/components/navbar/noAccount/homepageHeader.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 import { ReactNode } from 'react';
-import { BsQuestionSquareFill } from "react-icons/bs";
+import { PiQuestionMarkFill } from "react-icons/pi";
 import Image from 'next/image';
 
 
@@ -12,8 +12,8 @@ export default function Header({ children }: { children?: ReactNode }) {
   return (
     <div className="bg-green-500 text-black p-4 flex justify-between items-center relative z-10">
 
-      <button onClick={() => router.push('/info')} className="w-8 h-8 flex items-center justify-center">
-        <BsQuestionSquareFill size={24} />
+      <button onClick={() => router.push('/info')} className="w-8 h-8 flex items-center text-white justify-center">
+        <PiQuestionMarkFill size={40} />
       </button>
 
             <div className="absolute left-1/2 transform -translate-x-1/2 flex items-center space-x-2 text-xl font-bold">
